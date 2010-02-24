8dfd9ece8af15a2cb3ae981a985674fc942882c5
Noah Kantrowitz <noah@coderanger.net>
2010-02-24 15:22:36 -0500

    Add some more counters and a few more achievements.

--- achievements/ticket.py
+++ achievements/ticket.py
@@ -14,12 +14,19 @@ class TicketAchievementsProvider(Component):
 
     # ITicketChangeListener methods
     def ticket_created(self, tkt):
-        pass
+        if tkt['reporter'] != 'anonymous':
+            AchievementsSystem(self.env).update('ticket.created', tkt['reporter'], 1)
 
     def ticket_changed(self, tkt, comment, author, old_values):
-        if author != 'anonymous':
-            self.log.debug('TicketAchievementsProvider: Adding 1 to %s', author)
+        if author == 'anonymous':
+            return
+        if comment:
             AchievementsSystem(self.env).update('ticket.comments', author, 1)
+        if old_values[state] != tkt['state'] and tkt['state'] == 'fixed':
+            if tkt['resolution'] == 'fixed' tkt['reporter'] != 'anonymous':
+                AchievementsSystem(self.env).update('ticket.reported.closedfixed', tkt['reporter'], 1)
+            AchievementsSystem(self.env).update('ticket.closed', author, 1)
+                
 
     def ticket_deleted(self, tkt):
         pass
@@ -27,15 +34,27 @@ class TicketAchievementsProvider(Component):
     # AchievementsProvider methods
     def get_achievements(self):
         yield {
-            'name': 'ticket.comments_r1',
+            'name': 'ticket.comments.r1',
             'display': _('Comment on a ticket'),
             'counter': 'ticket.comments',
             'value': 1,
         }
         yield {
-            'name': 'ticket.comments_r2',
-            'display': _('Comment on a ticket twice'),
+            'name': 'ticket.comments.r2',
+            'display': _('Comment on 5 tickets'),
             'counter': 'ticket.comments',
-            'value': 2,
+            'value': 5,
+        }
+        yield {
+            'name': 'ticket.comments.r3',
+            'display': _('Comment on 10 tickets'),
+            'counter': 'ticket.comments',
+            'value': 10,
+        }
+        yield {
+            'name': 'ticket.created.r1',
+            'display': _('Create a ticket'),
+            'counter': 'ticket.created',
+            'value': 1,
         }
 
