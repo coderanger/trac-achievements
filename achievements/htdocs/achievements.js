9693bd7ee32083e7cf378ecb527864317692527b
Noah Kantrowitz <noah@coderanger.net>
2010-02-25 01:38:31 -0500

    Change the CSS class on the achievement popup, since I will probably want to use .achievement on the main UI page.

--- achievements/htdocs/achievements.js
+++ achievements/htdocs/achievements.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($){
   $.each(achievements, function(index, ach) {
-    $('<div class="achievement">'+ach.display+'</div>').appendTo('body');
+    $('<div class="achievement-popup">'+ach.display+'</div>').appendTo('body');
     setTimeout(function() {
       $('.achievement').fadeOut();
     }, 5000);
